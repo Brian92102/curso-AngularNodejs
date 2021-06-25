c83ed512b211c54cbcd81cf5ed5917adc7f02c82
Debo <deboxeneise@gmail.com>
2021-06-25 18:47:22 -0300

    album

--- curso-mean2/routes/album.js
+++ curso-mean2/routes/album.js
@@ -8,5 +8,9 @@ var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir: './uploads/album'});
 
 //api.get('/album', AlbumController.getAlbum);
+//api.get('/albums/:artist?', AlbumController.getAlbums);
+//api.post('/album', AlbumControler,saveAlbum);
+//api.put('/album/:id', AlbumController.updateAlbum);
+//api.delete('/album/:id',AlbumController.deleteAlbum);
 
 module.esport = api;
